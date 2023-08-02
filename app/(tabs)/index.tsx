52ce304a995b77fe3561d4cfae270345cb17683c
najadams <najmadams1706@gmail.com>
2023-08-02 03:45:45 +0000

    added the Tweet component to render tweets

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,26 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Image } from 'react-native';
 
 import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
+import { Tweet } from '../../components/Tweet';
+import tweets from '../../assets/data/tweets';
 
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
-  );
+   <View>
+       <Tweet tweet={tweets[0]}/>
+       <Tweet tweet={tweets[1]}/>
+       <Tweet tweet={tweets[2]}/>
+   </View>
+      );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'row',
+    padding:10,
+    borderBottomWidth:StyleSheet.hairlineWidth ,
+    borderColor: 'lightgrey'
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+  
 });
